c6cf7870d3be11f57df7d73087f33b30337a9789
zcensa <88883891+zcensa@users.noreply.github.com>
2023-06-03 13:52:45 +0200

    .

---
+++ amels200/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'common/carvariations.meta',
+	'common/contentunlocks.meta',
+	'common/dlctext.meta',
+	'common/handling.meta',
+	'common/vehicles.meta',
+	'common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'common/dlctext.meta'
+data_file 'HANDLING_FILE' 'common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'common/contentunlocks.meta'
