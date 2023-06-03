4d54f54807c8a212cf45c58d098ef75736d5a2a5
zcensa <88883891+zcensa@users.noreply.github.com>
2023-06-03 14:07:54 +0200

    .

---
+++ mach3/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'common/carvariations.meta',
+	'common/dlctext.meta',
+	'common/handling.meta',
+	'common/mach3contentunlocks.meta',
+	'common/shop_vehicle.meta',
+	'common/vehicles.meta',
+	'common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'common/dlctext.meta'
+data_file 'HANDLING_FILE' 'common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/carvariations.meta'
+data_file 'VEHICLE_SHOP_FILE' 'common/shop_vehicle.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'common/mach3contentunlocks.meta'
