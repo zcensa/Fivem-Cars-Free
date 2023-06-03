148023f60956427b4db7dce4da0664e09fb66676
zcensa <88883891+zcensa@users.noreply.github.com>
2023-06-03 13:57:38 +0200

    .

---
+++ cullinan2_kpym/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'common/carvariations.meta',
+	'common/handling.meta',
+	'common/vehicles.meta',
+	'common/*.meta'
+}
+data_file 'HANDLING_FILE' 'common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/carvariations.meta'
