e569c5dbba03a9ee4d8f1184271049158092426a
zcensa <88883891+zcensa@users.noreply.github.com>
2023-11-05 14:46:32 +0100

    .

---
+++ r8aud/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'common/dlctext.meta',
+	'common/handling.meta',
+	'common/shop_vehicle.meta',
+	'common/vehiclelayouts.meta',
+	'common/vehicles.meta',
+	'common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'common/dlctext.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'common/vehiclelayouts.meta'
+data_file 'HANDLING_FILE' 'common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/vehicles.meta'
+data_file 'VEHICLE_SHOP_FILE' 'common/shop_vehicle.meta'
