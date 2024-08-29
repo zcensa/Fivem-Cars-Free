efe0b626f2a84e137b362f8c8b3cd7b929a0ecfa
361Leooo <88883891+zcensa@users.noreply.github.com>
2024-08-29 08:52:36 +0200

    .

---
+++ Ürgentwie/23PorscheSUV/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+files { 
+    'data/**/*.meta',
+    'data/**/clip_sets.xml',
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'CLIP_SETS_FILE' 'data/**/clip_sets/clip_sets.xml'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
